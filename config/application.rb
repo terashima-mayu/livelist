469992ce40a9275fad2e687b80f7d4e1e136657a
mayu terashima <mtguitar0409@gmail.com>
2024-06-14 01:23:48 +0900

    デプロイに必要なファイルの追加

--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,6 @@ module Myapp
     #
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
-    #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
     config.hosts << 'https://livelist.onrender.com'
