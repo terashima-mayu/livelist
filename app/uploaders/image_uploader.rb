5d4855e1c66925f34178d5a4bdff1ba76d0b4c2b
mayu terashima <mtguitar0409@gmail.com>
2024-06-20 16:32:46 +0900

    セットリストの追加

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -13,10 +13,6 @@ class ImageUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
-  def default_url
-    'board_placeholder'
-  end
-
   def extension_allowlist
     %w[jpg jpeg gif png]
   end
