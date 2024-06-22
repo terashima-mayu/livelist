6edf496a7195532fd1c8adef4c4a7312cc324410
mayu terashima <mtguitar0409@gmail.com>
2024-06-23 00:09:09 +0900

    デプロイエラーの修正

--- config/initializers/spotify.rb
+++ config/initializers/spotify.rb
@@ -1,3 +1,2 @@
-unless Rails.env.production? && ENV['ASSET_PRECOMPILATION'] == 'true'
-  RSpotify::authenticate(ENV['SPOTIFY_CLIENT_ID'], ENV['SPOTIFY_CLIENT_SECRET'])
-end
+RSpotify::authenticate(ENV['SPOTIFY_CLIENT_ID'], ENV['SPOTIFY_CLIENT_SECRET'])
+
