996b759448b7af36085e3b5748d5d3b0eb8ec258
mayu terashima <mtguitar0409@gmail.com>
2024-06-22 23:46:28 +0900

    デプロイエラーの修正

--- config/initializers/spotify.rb
+++ config/initializers/spotify.rb
@@ -1,6 +1,3 @@
-if !(Rails.env.production? && ENV['ASSET_PRECOMPILATION'] == 'true')
-  Rails.logger.info "Authenticating with Spotify API"
+unless Rails.env.production? && ENV['ASSET_PRECOMPILATION'] == 'true'
   RSpotify::authenticate(ENV['SPOTIFY_CLIENT_ID'], ENV['SPOTIFY_CLIENT_SECRET'])
-else
-  Rails.logger.info "Skipping Spotify API authentication during asset precompilation"
 end
